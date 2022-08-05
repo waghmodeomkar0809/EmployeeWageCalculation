3b610a2b8fb9942ca074a7c08473e7517de8db4b
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-05 12:25:49 +0530

    new

---
+++ EmpWageCalculation/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCalculation
+{
+    internal class Employee
+    {
+        int isPrasent = 1;
+        Random random = new Random();
+
+        public void Attendence()
+        {
+           
+            int result = random.Next(0, 2);
+            if (result == isPrasent)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+           
+          
+            }
+        }
+    }
+}
