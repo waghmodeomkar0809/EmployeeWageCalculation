ea01cd272f00dda4c11392bcb8f98dfadeaed7b4
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-08-23 20:57:28 +0530

    LineComparison

--- EmpWageCalculation/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EmpWageCalculation;
-
-Employee empname= new Employee();
-empname.Attendence();
-int result=empname.DailyWage();
-Console.WriteLine(result);
